020802657d462385ef0eeaa9e1e90b6c2c6c1580
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2024-01-30 09:04:27 -0500

    sleepin

---
+++ sleepy.py
@@ -0,0 +1,21 @@
+import time
+
+from prefect import flow
+
+
+@flow(log_prints=True)
+def sleepy(seconds: int = 60):
+    print(f"sleeing for {seconds}")
+    time.sleep(seconds)
+
+
+if __name__ == "__main__":
+    sleepy.from_source(
+        source="https://github.com/kevingrismore/sleepy-flow.git",
+        entrypoint="sleepy.py:sleepy",
+    ).deploy(
+        name="azure-simple",
+        image="prefecthq/prefect:2-latest",
+        work_pool_name="azure-kubernetes",
+        build=False,
+    )
