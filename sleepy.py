0fce1cfbf5f5f9336b31c8c90be93f72f986f824
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2024-02-04 13:29:02 -0600

    add task

--- sleepy.py
+++ sleepy.py
@@ -1,13 +1,18 @@
 import time
 
-from prefect import flow
+from prefect import flow, task
 
 
 @flow(log_prints=True)
 def sleepy(seconds: int = 60, times: int = 1):
     for i in range(times):
-        print(f"sleeping for {seconds}")
-        time.sleep(seconds)
+        sleepy_task(seconds=seconds)
+
+
+@task
+def sleepy_task(seconds: int = 60):
+    print(f"sleeping for {seconds}")
+    time.sleep(seconds)
 
 
 if __name__ == "__main__":
@@ -21,12 +26,12 @@ if __name__ == "__main__":
         build=False,
     )
 
-    sleepy.from_source(
-        source="https://github.com/kevingrismore/sleepy-flow.git",
-        entrypoint="sleepy.py:sleepy",
-    ).deploy(
-        name="azure-sleepy-2",
-        image="prefecthq/prefect:2-latest",
-        work_pool_name="azure-kubernetes-default",
-        build=False,
-    )
+    # sleepy.from_source(
+    #     source="https://github.com/kevingrismore/sleepy-flow.git",
+    #     entrypoint="sleepy.py:sleepy",
+    # ).deploy(
+    #     name="azure-sleepy-2",
+    #     image="prefecthq/prefect:2-latest",
+    #     work_pool_name="azure-kubernetes-default",
+    #     build=False,
+    # )
