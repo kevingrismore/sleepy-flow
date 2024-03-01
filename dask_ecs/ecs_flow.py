61c98008ec6ae1a95d85f8ad892558fea39cfd14
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2024-03-01 16:24:24 -0600

    map one task

--- dask_ecs/ecs_flow.py
+++ dask_ecs/ecs_flow.py
@@ -13,19 +13,20 @@ def do_something(number: int):
 
 @flow(log_prints=True, task_runner=DaskTaskRunner)
 def ecs_flow():
-    do_something.map([1, 2, 3])
+    do_something.map([1])
 
 
 if __name__ == "__main__":
-    ecs_flow.from_source(
-        source="https://github.com/kevingrismore/sleepy-flow.git",
-        entrypoint="dask_ecs/ecs_flow.py:ecs_flow",
-    ).deploy(
-        name="ecs-flow",
-        work_pool_name="my-ecs-pool",
-        image=DeploymentImage(
-            name="kevingrismoreprefect/prefect-with-dask",
-            tag="2",
-            platform="linux/amd64",
-        ),
-    )
+    ecs_flow()
+    # ecs_flow.from_source(
+    #     source="https://github.com/kevingrismore/sleepy-flow.git",
+    #     entrypoint="dask_ecs/ecs_flow.py:ecs_flow",
+    # ).deploy(
+    #     name="ecs-flow",
+    #     work_pool_name="my-ecs-pool",
+    #     image=DeploymentImage(
+    #         name="kevingrismoreprefect/prefect-with-dask",
+    #         tag="2",
+    #         platform="linux/amd64",
+    #     ),
+    # )
