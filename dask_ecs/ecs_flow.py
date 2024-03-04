9299ea586f619da254cb9d2f74b014d73a93770d
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2024-03-04 10:48:42 -0600

    conditional import

--- dask_ecs/ecs_flow.py
+++ dask_ecs/ecs_flow.py
@@ -1,32 +1,26 @@
 from prefect import flow, task
 from prefect_dask import DaskTaskRunner
-from prefect.deployments import DeploymentImage
-
-from dask_ecs.util import do_something_useful
 
 
 @task
 def do_something(number: int):
+    try:
+        from util import do_something_useful
+
+        print("imported util during local run")
+    except ModuleNotFoundError:
+        from dask_ecs.util import do_something_useful
+
+        print("imported dask_ecs.util during deployment run")
     print(number)
     do_something_useful()
 
 
 @flow(log_prints=True, task_runner=DaskTaskRunner)
 def ecs_flow():
-    do_something.map([1])
+    do_something.submit(1)
 
 
 if __name__ == "__main__":
-    ecs_flow()
-    # ecs_flow.from_source(
-    #     source="https://github.com/kevingrismore/sleepy-flow.git",
-    #     entrypoint="dask_ecs/ecs_flow.py:ecs_flow",
-    # ).deploy(
-    #     name="ecs-flow",
-    #     work_pool_name="my-ecs-pool",
-    #     image=DeploymentImage(
-    #         name="kevingrismoreprefect/prefect-with-dask",
-    #         tag="2",
-    #         platform="linux/amd64",
-    #     ),
-    # )
+    ecs_flow.serve("dask-ecs-flow")
+    # ecs_flow()
